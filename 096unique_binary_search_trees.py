d31f44d2843b188bd80c1b0dbc0e604538c21d6d
chenxin6 <chenxin6@mail.ustc.edu.cn>
2024-10-10 10:01:39 +0800

    fix code style
    Signed-off-by: chenxin6 <chenxin6@mail.ustc.edu.cn>

--- 096unique_binary_search_trees.py
+++ 096unique_binary_search_trees.py
@@ -10,6 +10,26 @@ def get_unique_binary_search_trees_num(n):
     return res
 
 
+class Solution:
+    dp = []
+
+    def numTrees(self, n: int) -> int:
+        self.dp = [-1 for _ in range(n + 1)]
+        self.dp[0] = 1
+        self.dp[1] = 1
+        self.my_method(n)
+        return self.dp[n]
+
+    def my_method(self, n):
+        if self.dp[n] != -1:
+            return self.dp[n]
+        res = 0
+        for i in range(1, n + 1):
+            res += self.my_method(i - 1) * self.my_method(n - i)
+        self.dp[n] = res
+        return res
+
+
 n = 2
 dp = [-1 for _ in range(n + 1)]
 print(get_unique_binary_search_trees_num(n))
--- 23merge_k_sorted_lists.py
+++ 23merge_k_sorted_lists.py
@@ -9,18 +9,18 @@ class ListNode:
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        arr = self.myMethod(lists)
+        arr = self.my_method(lists)
         n = len(arr)
         root = ListNode(0)
-        tempNode = root
-        while n > 0 and type(arr[0]) is ListNode:
-            tempNode.next = ListNode(arr[0].val)
-            tempNode = tempNode.next
+        temp_node = root
+        while n > 0 and isinstance(arr[0], ListNode):
+            temp_node.next = ListNode(arr[0].val)
+            temp_node = temp_node.next
             arr[0] = arr[0].next
             self.adjust(arr, 0, n)
         return root.next
 
-    def myMethod(self, lists: List[Optional[ListNode]]) -> List[Optional[ListNode]]:
+    def my_method(self, lists: List[Optional[ListNode]]) -> List[Optional[ListNode]]:
         arr = []
         for node in lists:
             arr.append(node)
@@ -30,28 +30,28 @@ class Solution:
         return arr
 
     def adjust(self, arr, i, n):
-        fatherValue = None
+        father_value = None
         if arr[i] is not None:
-            fatherValue = arr[i]
-        while self.getLeftChild(i) < n:
-            child = self.getLeftChild(i)
+            father_value = arr[i]
+        while self.get_left_child(i) < n:
+            child = self.get_left_child(i)
             if child + 1 < n and (arr[child] is None or (arr[child + 1] is not None and arr[child].val > arr[child + 1].val)):
                 child += 1
-            if fatherValue is not None and arr[child] is not None and fatherValue.val <= arr[child].val:
+            if father_value is not None and arr[child] is not None and father_value.val <= arr[child].val:
                 break
             elif arr[child] is None:
                 break
             else:
                 self.swap(arr, i, child)
                 i = child
-        arr[i] = fatherValue
+        arr[i] = father_value
 
     def swap(self, arr, i, j):
         temp = arr[i]
         arr[i] = arr[j]
         arr[j] = temp
 
-    def getLeftChild(self, i):
+    def get_left_child(self, i):
         return 2 * i + 1
 
 
