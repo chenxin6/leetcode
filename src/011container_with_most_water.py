7866ad05d8ca640ee3099bd3545d989b9a6ca7c9
chenxin6 <chenxin6@mail.ustc.edu.cn>
2026-08-17 14:52:08 +0800

    fix test
    Signed-off-by: chenxin6 <chenxin6@mail.ustc.edu.cn>

--- src/011container_with_most_water.py
+++ src/011container_with_most_water.py
@@ -1,33 +1,31 @@
-def get_water_area(arr):
-    start = 0
-    end = len(arr) - 1
-    res = 0
-    while start < end:
-        temp = (end - start) * get_min(arr[start], arr[end])
-        if temp > res:
-            res = temp
-        if arr[start] < arr[end]:
-            start = start + 1
-        else:
-            end = end - 1
-    return res
+from typing import List
 
 
-def get_water_area2(arr):
-    res = 0
-    for i in range(len(arr)):
-        for j in range(i + 1, len(arr)):
-            temp = (j - i) * get_min(arr[i], arr[j])
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        arr = height
+        start = 0
+        end = len(arr) - 1
+        res = 0
+        while start < end:
+            temp = (end - start) * min(arr[start], arr[end])
             if temp > res:
                 res = temp
-    return res
+            if arr[start] < arr[end]:
+                start = start + 1
+            else:
+                end = end - 1
+        return res
+
+    def maxArea2(self, height: List[int]) -> int:
+        arr = height
+        res = 0
+        for i in range(len(arr)):
+            for j in range(i + 1, len(arr)):
+                temp = (j - i) * min(arr[i], arr[j])
+                if temp > res:
+                    res = temp
+        return res
 
 
-def get_min(i, j):
-    if i >= j:
-        return j
-    else:
-        return i
-
-
-print(get_water_area([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+print(Solution().maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
